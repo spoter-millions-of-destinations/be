8df3c55e9699346cc66735717a2b49edc2b7d609
Cam Nguyen <cam2362002@gmail.com>
2024-07-27 15:43:51 +0700

    feat: get ad posts

--- db/repositories/post.repository.ts
+++ db/repositories/post.repository.ts
@@ -19,13 +19,15 @@ export class PostRepository extends Repository<Post> {
     return post;
   }
 
-  async getPosts(query: GetPostsParamsDto): Promise<[Post[], number]> {
+  async getNormalPosts(query: GetPostsParamsDto): Promise<[Post[], number]> {
+    const limit = query.limit - (query.limit / 5);
     const qb = this.createQueryBuilder('post')
       .leftJoinAndSelect('post.user', 'user')
       .leftJoinAndSelect('post.attraction', 'attraction')
       .where('post.description ILIKE :search', { search: `%${query.search ?? ''}%` })
+      .andWhere('attraction.advertisingPackageId IS NULL')
       .skip(query.offset)
-      .take(query.limit);
+      .take(limit);
 
       query.rate && qb.andWhere('post.rate = :rate', { rate: query.rate });
 
@@ -53,5 +55,31 @@ export class PostRepository extends Repository<Post> {
       }
       
     return await qb.getManyAndCount();
+  }
+
+  async getAdvertisingPosts(query: GetPostsParamsDto): Promise<[Post[], number]> {
+    const limit = query.limit / 5;
+    const qb = this.createQueryBuilder('post')
+      .leftJoinAndSelect('post.user', 'user')
+      .leftJoinAndSelect('post.attraction', 'attraction')
+      .where('attraction.advertisingPackageId IS NOT NULL')
+      .orderBy('post.createdAt', 'DESC')
+      .skip(query.offset)
+      .take(limit);
+      
+    return await qb.getManyAndCount();
+  }
+
+  async getPosts(query: GetPostsParamsDto): Promise<[Post[], number]> {
+    const normalPosts = await this.getNormalPosts(query);
+    const advertisingPosts = await this.getAdvertisingPosts(query);
+    const numOfAdvertisingPosts = advertisingPosts[0].length;
+    const posts = [...normalPosts[0]];
+
+    for (let i = 1; i <= numOfAdvertisingPosts; i++) {
+      posts.splice((i * 5) - 1, 0, advertisingPosts[0][i-1]);
+    }
+
+    return [posts, normalPosts[1] + advertisingPosts[1]];
   }
 }
